3d60eeabee8e649fbf2e761a2e336840e4d30436
Lars Holmgren <lars.holmgren@electec.se>
2025-01-30 16:39:00 +0100

    Again

--- pisetup/bootstrap.sh
+++ pisetup/bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 echo "Setting up vlan"      
 sudo apt install vlan -y
 echo "Copying vlan config to /etc/network/interfaces.d"
-sudo mkdir /etc/network/interfaces.d/vlan
+sudo mkdir -p /etc/network/interfaces.d/vlan
 sudo cp configfiles/vlans /etc/network/interfaces.d/vlan
 
 
